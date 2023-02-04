7f5a18b6395cfa44e87887ba54319a95ad759eb0
dat-adi-work <dat.adithya@gmail.com>
2023-02-04 18:20:32 +0530

    feat: added in accounts
    Testing, documentation and the templates have been completed.

--- lil_bank/accounts/urls.py
+++ lil_bank/accounts/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 app_name = "accounts"
 
-urlpatterns = []
+urlpatterns = [
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('signup/', views.SignUpView.as_view(), name="signup"),
+    path('logout/', views.LogoutView.as_view(), name="logout")
+]
--- lil_bank/accounts/views.py
+++ lil_bank/accounts/views.py
@@ -1,2 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+
+class LoginView(TemplateView):
+    """
+    This is the view for logging in.
+    """
+    template_name = "accounts/login.html"
+
+
+class SignUpView(TemplateView):
+    """
+    This is the view for signing in.
+    """
+    template_name = "accounts/signup.html"
+
+
+class LogoutView(TemplateView):
+    """
+    This is the view for logging out.
+    """
+    template_name = "accounts/logout.html"
