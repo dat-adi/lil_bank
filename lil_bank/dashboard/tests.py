9f173ccf221e783f2d5ada6db12bce1543c24e81
dat-adi-work <dat.adithya@gmail.com>
2023-02-04 15:04:57 +0530

    tests: added status check for dashboard
    The __init__.py file should not be present in the root directory.
    This turns the project into a package which is what messes up the
    testing environment.

--- lil_bank/dashboard/tests.py
+++ lil_bank/dashboard/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+"""
+This file is for testing the status of the dashboard page.
+"""
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class TestStatus(TestCase):
+    def test_status(self):
+        c: Client = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200, "The page seems to be down.")
