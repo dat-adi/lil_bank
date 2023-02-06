a66f7e32b5da246f21336f5d286d0fab8dace60f
Aritra Sarkar <aritra.sarkar@bajajfinserv.in>
2023-02-06 06:51:18 +0000

    migrate accounts models with postgresql

--- lil_bank/accounts/models.py
+++ lil_bank/accounts/models.py
@@ -1,1 +1,9 @@
-from django.db import models
+from django.db.models import Model, AutoField, CharField
+
+class Customer(Model):
+    customerid: AutoField = AutoField(primary_key=True)
+    accountno: CharField = CharField(max_length=256)
+    firstname: CharField = CharField(max_length=32)
+    lastname: CharField = CharField(max_length=32)
+    address: CharField = CharField(max_length=512)
+    phone: CharField = CharField(max_length=16)
