a0709e825a68f6bf47de1701669f86228596c6e3
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-02-06 15:12:49 +0530

    Login corrected.

--- lil_bank/accounts/views.py
+++ lil_bank/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .forms import LoginForm, SignUpForm
 from django.contrib.auth import login, logout
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from .models import Account, Customer
+# from dashboard import views as dashboard_views
 
 # class LoginForm(forms.Form):
 #     """
@@ -36,7 +37,10 @@ class LoginView(TemplateView):
                 if user.check_password(form.cleaned_data['password']):
                     # Log in the user.
                     login(request, user)
-                    return render(request, "accounts/login_success.html")
+                    # return render(request, "accounts/login_success.html")
+                    # Now redirect to the home page.
+                    return redirect('dashboard:landing_page')
+                    # return render(request, "accounts/login_success.html")
             return render(request, "accounts/login_fail.html")
     
 
@@ -91,7 +95,7 @@ class LogoutView(TemplateView):
     """
     This is the view for logging out.
     """
-    template_name = "accounts/logout.html"
+    template_name = "logout.html"
     # This is the view for logging out. It logs out the user and redirects to the login page.
     def get(self, request):
         # Log out the user.
