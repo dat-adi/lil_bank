4b808046c58027a1600e1e2462d9ccd6aa2025ac
Jishnu Kumar Srivastava <jisheras31@gmail.com>
2023-02-06 17:09:09 +0530

    User Profile Page - almost done!

--- lil_bank/accounts/urls.py
+++ lil_bank/accounts/urls.py
@@ -6,5 +6,6 @@ app_name = "accounts"
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
     path('signup/', views.SignUpView.as_view(), name="signup"),
-    path('logout/', views.LogoutView.as_view(), name="logout")
+    path('logout/', views.LogoutView.as_view(), name="logout"),
+    path('user_profile/', views.UserProfile.as_view(), name="user_profile"),
 ]
