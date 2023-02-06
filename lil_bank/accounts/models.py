8b483164ba4f2be5ba6bc229d04e3ede63c33e32
dat-adi-work <dat.adithya@gmail.com>
2023-02-06 20:16:46 +0530

    feat: updated the navbar with new routes to hit

--- lil_bank/accounts/models.py
+++ lil_bank/accounts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Customer(models.Model):
     id: models.CharField = models.CharField(primary_key=True, max_length=32)
     first_name: models.CharField = models.CharField(max_length=32)
     last_name: models.CharField = models.CharField(max_length=32)
     address: models.CharField = models.CharField(max_length=256)
     phone: models.CharField = models.CharField(max_length=16)
+
 
 class Account(models.Model):
     no: models.IntegerField = models.IntegerField(primary_key=True)
